f85c166807a841c1f80e634ba5aee8a4367805f3
Deniz Kusefoglu <ebb-tide@github.com>
2025-02-19 22:32:35 -0800

    no confirm changeset on deploy

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,12 @@ source .env
 sam deploy \
   --template-file template.yaml \
   --stack-name telegram-calendar-bot \
+  --no-confirm-changeset \
+  --no-fail-on-empty-changeset \
   --parameter-overrides \
     GoogleClientSecret="$GOOGLE_CLIENT_SECRET" \
     DynamoDBTable="$DYNAMODB_TABLE" \
     TelegramBotToken="$TELEGRAM_BOT_TOKEN" \
     GoogleClientId="$GOOGLE_CLIENT_ID" \
     OpenAIApiKey="$OPENAI_API_KEY" \
-    GoogleRedirectUri="$GOOGLE_REDIRECT_URI"./
+    GoogleRedirectUri="$GOOGLE_REDIRECT_URI"
